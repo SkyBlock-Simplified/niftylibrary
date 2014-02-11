694005e33303e0d724a7cb2e68f7d00561aee59b
Brian Graham <support@netcoding.net>
2014-02-10 20:34:13 -0500

    Test commit

--- src/net/netcoding/niftybukkit/NiftyBukkit.java
+++ src/net/netcoding/niftybukkit/NiftyBukkit.java
@@ -16,6 +16,7 @@ public class NiftyBukkit extends JavaPlugin implements Listener {
 
 	@Override
 	public void onEnable() {
+		// test commit
 		plugin = this;
 		new BungeeHelper().register();
 		Bukkit.getMessenger().registerOutgoingPluginChannel(this, BungeeHelper.BUNGEE_CHANNEL);
