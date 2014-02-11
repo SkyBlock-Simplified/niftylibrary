57380cbdcf3317fd41e0840ed2f81c36e4710846
Brian Graham <support@netcoding.net>
2014-02-10 20:34:40 -0500

    Removed test commit comment

--- src/net/netcoding/niftybukkit/NiftyBukkit.java
+++ src/net/netcoding/niftybukkit/NiftyBukkit.java
@@ -16,7 +16,6 @@ public class NiftyBukkit extends JavaPlugin implements Listener {
 
 	@Override
 	public void onEnable() {
-		// test commit
 		plugin = this;
 		new BungeeHelper().register();
 		Bukkit.getMessenger().registerOutgoingPluginChannel(this, BungeeHelper.BUNGEE_CHANNEL);
