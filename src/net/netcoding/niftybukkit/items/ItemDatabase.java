d03e628d08ba813124e2232b9593cdc463d10b8e
Brian Graham <support@netcoding.net>
2014-03-10 11:15:02 -0400

    Updated name storage

--- src/net/netcoding/niftybukkit/items/ItemDatabase.java
+++ src/net/netcoding/niftybukkit/items/ItemDatabase.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -246,15 +247,12 @@ public class ItemDatabase extends BukkitHelper {
 			this.items.put(itemName, numeric);
 			ItemData itemData = new ItemData(numeric, data);
 
-			if (this.names.containsKey(itemData)) {
-				List<String> nameList = this.names.get(itemData);
-				nameList.add(itemName);
-				Collections.sort(nameList, compare);
-			} else {
-				List<String> nameList = new ArrayList<String>();
-				nameList.add(itemName);
-				this.names.put(itemData, nameList);
+			if (!this.names.containsKey(itemData)) {
+				this.names.put(itemData, new ArrayList<String>(Arrays.asList(itemName)));
 				this.primaryName.put(itemData, itemName);
+			} else {
+				this.names.get(itemData).add(itemName);
+				Collections.sort(this.names.get(itemData), compare);
 			}
 		}
 	}
