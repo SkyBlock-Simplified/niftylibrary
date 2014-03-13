459edfde1722101ba86436ce5053fc08533aa16a
Brian Graham <support@netcoding.net>
2014-03-13 11:55:24 -0400

    Added extended javaplugin for easy log and method helper access

---
+++ src/net/netcoding/niftybukkit/minecraft/BukkitPlugin.java
@@ -0,0 +1,67 @@
+package net.netcoding.niftybukkit.minecraft;
+
+import net.netcoding.niftybukkit.util.StringUtil;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitPlugin extends JavaPlugin {
+
+	private final transient Log log;
+
+	public BukkitPlugin() {
+		this.log = new Log(this);
+	}
+
+	public Log getLog() {
+		return this.log;
+	}
+
+	public boolean hasPermissions(CommandSender sender, String... permissions) {
+		if (isConsole(sender)) return true;
+		return sender.hasPermission(String.format("%s.%s", this.getDescription().getName(), StringUtil.implode(".", permissions)));
+	}
+
+	public static boolean isConsole(CommandSender sender) {
+		return !isPlayer(sender);
+	}
+
+	public static boolean isConsole(String senderName) {
+		return !isPlayer(senderName);
+	}
+
+	public static boolean isPlayer(CommandSender sender) {
+		return isPlayer(sender.getName());
+	}
+
+	public static boolean isPlayer(String senderName) {
+		return !senderName.equalsIgnoreCase("console");
+	}
+
+	public static Player findPlayer(String playerName) {
+		Player player = Bukkit.getPlayer(playerName);
+
+		if (player != null)
+			return player;
+		else {
+			for (Player iPlayer : Bukkit.getOnlinePlayers()) {
+				if (iPlayer.getName().toLowerCase().startsWith(playerName.toLowerCase()))
+					return iPlayer;
+			}
+
+			return null;
+		}
+	}
+
+	public static String findPlayerName(String playerName) {
+		Player player = findPlayer(playerName);
+
+		if (player != null)
+			return player.getName();
+		else
+			return playerName;
+	}
+
+}
