46f4bf17575aff71766ba21c0dbdda462644e8da
Brian Graham <support@netcoding.net>
2014-04-22 01:16:59 -0400

    Moved basic sql methods to factory class

---
+++ src/net/netcoding/niftybukkit/database/pooling/ConnectionFactory.java
@@ -0,0 +1,62 @@
+package net.netcoding.niftybukkit.database.pooling;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionFactory {
+
+	private final String url;
+	private final Properties properties;
+
+	/**
+	 * @param url  Database URL.
+	 * @param user Username for the database connection.
+	 * @param pass Password for the database connection.
+	 */
+	public ConnectionFactory(String url, String user, String pass) {
+		Properties properties = new Properties();
+		properties.put("user", user);
+		properties.put("password", pass);
+		this.properties = properties;
+		this.url = url;
+	}
+
+	/**
+	 * @param url        Database URL
+	 * @param properties Properties of the connection to establish.
+	 */
+	public ConnectionFactory(String url, Properties properties) {
+		this.url = url;
+		this.properties = properties;
+	}
+
+
+	/**
+	 * Returns a connection to the database.
+	 *
+	 * @return Connection to the database.
+	 * @throws SQLException When connection is not available.
+	 */
+	protected Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(this.getUrl(), this.getProperties());
+	}
+
+	protected Properties getProperties() {
+		return this.properties;
+	}
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public boolean isValidConnection() {
+		try (Connection connection = this.getConnection()) {
+			return true;
+		} catch (SQLException ex) {
+			return false;
+		}
+	}
+
+}
