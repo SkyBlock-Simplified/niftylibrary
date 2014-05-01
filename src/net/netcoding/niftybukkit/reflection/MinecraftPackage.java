d4b721b4b4ab010fbfae3f9d6f1b98553f5da7fb
Brian Graham <support@netcoding.net>
2014-05-01 19:18:08 -0400

    Added some newlines

--- src/net/netcoding/niftybukkit/reflection/MinecraftPackage.java
+++ src/net/netcoding/niftybukkit/reflection/MinecraftPackage.java
@@ -3,16 +3,19 @@ package net.netcoding.niftybukkit.reflection;
 import org.bukkit.Bukkit;
 
 public enum MinecraftPackage {
+
 	MINECRAFT_SERVER {
 		@Override
 		public String toString() {
 			return "net.minecraft.server." + Bukkit.getServer().getClass().getPackage().getName().substring(23, 30);
 		}
 	},
+
 	CRAFTBUKKIT {
 		@Override
 		public String toString() {
 			return Bukkit.getServer().getClass().getPackage().getName();
 		}
 	};
+
 }
