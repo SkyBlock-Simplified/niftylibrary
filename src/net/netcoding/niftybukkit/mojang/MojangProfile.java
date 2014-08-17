bd11131665a9281c007f0a0bddc711a4862a6f06
Brian Graham <support@netcoding.net>
2014-08-17 00:09:58 -0400

    Remove public constructor Privatize default constructor Add expiry support for Mojangs rate limit Add profile to 10 minute cache on first access - needs more work Make use of expiry in expiry and hashCode methods

--- src/net/netcoding/niftybukkit/mojang/MojangProfile.java
+++ src/net/netcoding/niftybukkit/mojang/MojangProfile.java
@@ -10,12 +10,10 @@ public class MojangProfile {
 	private String id;
 	private UUID uuid;
 	private String name;
+	private int updated = (int)(System.currentTimeMillis() / 1000);
 	private static final Pattern UUID_FIX = Pattern.compile("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})");
 
-	public MojangProfile(String name, String id) {
-		this.name = name;
-		this.id = id.replace("-", "");
-	}
+	private MojangProfile() { }
 
 	@Override
 	public boolean equals(Object obj) {
@@ -25,12 +23,15 @@ public class MojangProfile {
 		MojangProfile profile = (MojangProfile)obj;
 		if (!this.getUniqueId().equals(profile.getUniqueId())) return false;
 		if (!this.getName().equals(profile.getName())) return false;
+		if (this.updated > profile.updated) return false; 
 		return true;
 	}
 
 	public UUID getUniqueId() {
-		if (this.uuid == null)
+		if (this.uuid == null) {
 			this.uuid = UUID.fromString(UUID_FIX.matcher(this.id.replace("-", "")).replaceAll("$1-$2-$3-$4-$5"));
+			MojangRepository.cache.add(this);
+		}
 
 		return this.uuid;
 	}
@@ -39,12 +40,17 @@ public class MojangProfile {
 		return this.name;
 	}
 
+	public boolean hasExpired() {
+		return System.currentTimeMillis() / 1000 - this.updated >= 600;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + (StringUtil.isEmpty(this.getName()) ? 0 : this.getName().hashCode());
 		result = prime * result + (StringUtil.isEmpty(this.id) ? 0 : this.getUniqueId().hashCode());
+		result = prime * result + this.updated;
 		return result;
 	}
 
