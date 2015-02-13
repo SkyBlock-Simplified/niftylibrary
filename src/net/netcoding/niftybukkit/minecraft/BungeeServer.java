5a21af3de54e8e68a5ad71f1de99ff043a4bc555
Brian Graham <support@netcoding.net>
2015-02-13 12:30:21 -0500

    Remove temporary player list

--- src/net/netcoding/niftybukkit/minecraft/BungeeServer.java
+++ src/net/netcoding/niftybukkit/minecraft/BungeeServer.java
@@ -1,15 +1,10 @@
 package net.netcoding.niftybukkit.minecraft;
 
 import java.net.InetSocketAddress;
-import java.util.Set;
-
-import net.netcoding.niftybukkit.mojang.MojangProfile;
-import net.netcoding.niftybukkit.util.concurrent.ConcurrentSet;
 
 public class BungeeServer extends MinecraftServer {
 
 	boolean loadedOnce = false;
-	Set<MojangProfile> tempList = new ConcurrentSet<>();
 
 	BungeeServer(String serverName) {
 		this.serverName = serverName;
