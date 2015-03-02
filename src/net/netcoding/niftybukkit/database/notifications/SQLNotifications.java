cb567e7661579d749444d652238eb37bb5ea9832
Brian Graham <support@netcoding.net>
2015-03-02 16:42:17 -0500

    Finished documentation Renamed addDatabaseListener to addListener Removed stopListener in favor of removeListener

--- src/net/netcoding/niftybukkit/database/notifications/SQLNotifications.java
+++ src/net/netcoding/niftybukkit/database/notifications/SQLNotifications.java
@@ -6,41 +6,90 @@ import java.util.Properties;
 import net.netcoding.niftybukkit.NiftyBukkit;
 import net.netcoding.niftybukkit.database.pooling.SQLPooling;
 import net.netcoding.niftybukkit.util.StringUtil;
-import net.netcoding.niftybukkit.util.concurrent.ConcurrentList;
+import net.netcoding.niftybukkit.util.concurrent.ConcurrentSet;
 
 import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitTask;
 
-public class SQLNotifications extends SQLPooling {
+/**
+ * Adds support for notifications through an sql server instance.
+ */
+public abstract class SQLNotifications extends SQLPooling implements Runnable {
 
 	static final String ACTIVITY_TABLE = "niftybukkit_activity";
 	private static final int DEFAULT_DELAY = 10;
-	private final transient ConcurrentList<DatabaseNotification> listeners = new ConcurrentList<>();
+	private final transient ConcurrentSet<DatabaseNotification> listeners = new ConcurrentSet<>();
 	private transient BukkitTask task;
 
-	public SQLNotifications(String url, String user, String pass) throws SQLException {
-		super(url, user, pass);
-	}
-
+	/**
+	 * Create a new notification instance.
+	 * 
+	 * @param url        Database connection url.
+	 * @param properties Properties of the database connection.
+	 * @throws SQLException
+	 */
 	public SQLNotifications(String url, Properties properties) throws SQLException {
 		super(url, properties);
 	}
 
-	public void addDatabaseListener(String table, DatabaseListener notifier) throws SQLException, Exception {
-		this.addDatabaseListener(table, notifier, DEFAULT_DELAY, false);
+	/**
+	 * Create a new notification instance.
+	 * 
+	 * @param url  Database connection url.
+	 * @param user Username of the database connection.
+	 * @param pass Password of the database connection.
+	 * @throws SQLException
+	 */
+	public SQLNotifications(String url, String user, String pass) throws SQLException {
+		super(url, user, pass);
 	}
 
-	public void addDatabaseListener(String table, DatabaseListener notifier, long delay) throws SQLException, Exception {
-		this.addDatabaseListener(table, notifier, delay, false);
+	/**
+	 * Add a listener on the given table.
+	 * 
+	 * @param table    Table name to listen to.
+	 * @param notifier Listener to send notifications to.
+	 * @throws SQLException
+	 */
+	public void addListener(String table, DatabaseListener notifier) throws SQLException {
+		this.addListener(table, notifier, DEFAULT_DELAY, false);
 	}
 
-	public void addDatabaseListener(String table, DatabaseListener notifier, boolean overwrite) throws SQLException, Exception {
-		this.addDatabaseListener(table, notifier, DEFAULT_DELAY, overwrite);
+	/**
+	 * Add a listener on the given table.
+	 * 
+	 * @param table     Table name to listen to.
+	 * @param notifier  Listener to send notifications to.
+	 * @param overwrite True to overwrite the triggers in the database, otherwise false.
+	 * @throws SQLException
+	 */
+	public void addListener(String table, DatabaseListener notifier, boolean overwrite) throws SQLException {
+		this.addListener(table, notifier, DEFAULT_DELAY, overwrite);
 	}
 
-	public void addDatabaseListener(String table, DatabaseListener notifier, long delay, boolean overwrite) throws SQLException, Exception {
-		DatabaseNotification listener = new DatabaseNotification(this, table, notifier, delay, overwrite);
-		this.listeners.add(listener);
+	/**
+	 * Add a listener on the given table.
+	 * 
+	 * @param table    Table name to listen to.
+	 * @param notifier Listener to send notifications to.
+	 * @param delay    How long in ticks to wait before checking.
+	 * @throws SQLException
+	 */
+	public void addListener(String table, DatabaseListener notifier, long delay) throws SQLException {
+		this.addListener(table, notifier, delay, false);
+	}
+
+	/**
+	 * Add a listener on the given table.
+	 * 
+	 * @param table     Table name to listen to.
+	 * @param notifier  Listener to send notifications to.
+	 * @param delay     How long in ticks to wait before checking.
+	 * @param overwrite True to overwrite the triggers in the database, otherwise false.
+	 * @throws SQLException
+	 */
+	public void addListener(String table, DatabaseListener notifier, long delay, boolean overwrite) throws SQLException {
+		this.listeners.add(new DatabaseNotification(this, table, notifier, delay, overwrite));
 		this.createLogTable();
 		this.createPurgeEvent();
 		if (this.task == null) this.task = Bukkit.getScheduler().runTaskTimerAsynchronously(NiftyBukkit.getPlugin(), this, 0, delay);
@@ -54,13 +103,40 @@ public class SQLNotifications extends SQLPooling {
 		update(StringUtil.format("CREATE EVENT IF NOT EXISTS `purgeNiftyNotifications` ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 1 DAY DO DELETE LOW_PRIORITY FROM `{0}`.`niftybukkit_activity` WHERE `time` < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 7 DAY));", this.getSchema()));
 	}
 
+	/**
+	 * Remove all listeners.
+	 */
+	public void removeListeners() {
+		this.removeListener(null);
+	}
+
+	/**
+	 * Remove all listeners.
+	 * 
+	 * @param dropTriggers True to drop triggers, otherwise false.
+	 */
+	public void removeListeners(boolean dropTriggers) {
+		this.removeListener(null, dropTriggers);
+	}
+
+	/**
+	 * Remove listener from the given table.
+	 * 
+	 * @param table Table name to remove listeners from.
+	 */
 	public void removeListener(String table) {
 		this.removeListener(table, false);
 	}
 
+	/**
+	 * Remove listener from the given table.
+	 * 
+	 * @param table        Table name to remove listeners from.
+	 * @param dropTriggers True to drop triggers, otherwise false.
+	 */
 	public void removeListener(String table, boolean dropTriggers) {
 		for (DatabaseNotification listener : this.listeners) {
-			if (listener.getTable().equals(table))
+			if (listener.getTable().equals(table) || StringUtil.isEmpty(table))
 				listener.stop(dropTriggers);
 		}
 
@@ -70,20 +146,6 @@ public class SQLNotifications extends SQLPooling {
 				this.task = null;
 			}
 		}
-	}
-
-	public void stopListening() {
-		this.stopListening(false);
-	}
-
-	public void stopListening(boolean dropTriggers) {
-		if (this.task != null) {
-			this.task.cancel();
-			this.task = null;
-		}
-
-		for (DatabaseNotification listener : this.listeners)
-			listener.stop(dropTriggers);
 	}
 
 	@Override
