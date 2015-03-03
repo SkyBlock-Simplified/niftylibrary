1bdf998053683d8a5b7be2802e0798463921a6eb
Brian Graham <support@netcoding.net>
2015-03-02 21:07:26 -0500

    Sorted members/imports

--- src/net/netcoding/niftybukkit/signs/SignListener.java
+++ src/net/netcoding/niftybukkit/signs/SignListener.java
@@ -1,6 +1,9 @@
 package net.netcoding.niftybukkit.signs;
 
-import net.netcoding.niftybukkit.signs.events.*;
+import net.netcoding.niftybukkit.signs.events.SignBreakEvent;
+import net.netcoding.niftybukkit.signs.events.SignCreateEvent;
+import net.netcoding.niftybukkit.signs.events.SignInteractEvent;
+import net.netcoding.niftybukkit.signs.events.SignUpdateEvent;
 
 public interface SignListener {
 
