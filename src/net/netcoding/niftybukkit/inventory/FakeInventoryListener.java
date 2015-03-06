97c9dc641a9bde15b1fd40a925bcd09be667038a
Brian Graham <support@netcoding.net>
2015-03-06 17:01:11 -0500

    Use custom inventory events

--- src/net/netcoding/niftybukkit/inventory/FakeInventoryListener.java
+++ src/net/netcoding/niftybukkit/inventory/FakeInventoryListener.java
@@ -1,15 +1,18 @@
 package net.netcoding.niftybukkit.inventory;
 
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
+import net.netcoding.niftybukkit.inventory.events.InventoryClickEvent;
+import net.netcoding.niftybukkit.inventory.events.InventoryCloseEvent;
+import net.netcoding.niftybukkit.inventory.events.InventoryItemInteractEvent;
+import net.netcoding.niftybukkit.inventory.events.InventoryOpenEvent;
 
 public interface FakeInventoryListener {
 
-	public void onInventoryClose(InventoryCloseEvent event);
-
 	public void onInventoryClick(InventoryClickEvent event);
 
+	public void onInventoryClose(InventoryCloseEvent event);
+
 	public void onInventoryOpen(InventoryOpenEvent event);
+
+	public void onInventoryItemInteract(InventoryItemInteractEvent event);
 
 }
