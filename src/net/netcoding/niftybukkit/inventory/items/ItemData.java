be19942220964b8d768c55fbc8181af9a217c116
Brian Graham <support@netcoding.net>
2015-03-10 23:22:05 -0400

    Now supports glowing items Added method to grab the item from ItemData

--- src/net/netcoding/niftybukkit/inventory/items/ItemData.java
+++ src/net/netcoding/niftybukkit/inventory/items/ItemData.java
@@ -1,38 +1,99 @@
 package net.netcoding.niftybukkit.inventory.items;
 
+import net.netcoding.niftybukkit.reflection.MinecraftPackage;
+import net.netcoding.niftybukkit.reflection.Reflection;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+
+@SuppressWarnings("deprecation")
 public class ItemData {
 
-	private final int itemId;
-	private final short itemData;
+	private int id = 0;
+	private short data = 0;
+	private boolean glow = false;
 
-	public ItemData(int itemId) {
-		this(itemId, (short)0);
+	public ItemData(ItemStack item) {
+		this(item.getTypeId(), item.getDurability());
 	}
 
-	public ItemData(int itemId, short itemData) {
-		this.itemId = itemId;
-		this.itemData = itemData;
+	public ItemData(int id) {
+		this(id, (short)0);
+	}
+
+	public ItemData(int id, short data) {
+		this.id = id;
+		this.data = data;
+	}
+
+	public void addGlow() {
+		this.glow = true;
+	}
+
+	public final static ItemStack addGlow(ItemStack item) {
+		try {
+			item.addUnsafeEnchantment(Enchantment.DURABILITY, -1);
+			Reflection craftItemStack = new Reflection("CraftItemStack", "inventory", MinecraftPackage.CRAFTBUKKIT);
+			Reflection nmsItemStack = new Reflection("ItemStack", MinecraftPackage.MINECRAFT_SERVER);
+			Reflection tagCompound = new Reflection("NBTTagCompound", MinecraftPackage.MINECRAFT_SERVER);
+			Object itemStack = craftItemStack.invokeMethod("asNMSCopy", null, item);
+			Object tag = nmsItemStack.invokeMethod("getTag", itemStack);
+
+			if (tag == null) {
+				tag = tagCompound.newInstance();
+				nmsItemStack.invokeMethod("setTag", itemStack, tag);
+				tag = nmsItemStack.invokeMethod("getTag", itemStack);
+			}
+
+
+			if (MinecraftPackage.MINECRAFT_VERSION.toString().startsWith("v1_7"))
+				tagCompound.invokeMethod("set", tag, "ench", new Reflection("NBTTagList", MinecraftPackage.MINECRAFT_SERVER).newInstance());
+			else {
+				if (!(boolean)tagCompound.invokeMethod("hasKey", tag, "HideFlags"))
+					tagCompound.invokeMethod("setInt", tag, "HideFlags", 1);
+			}
+
+			nmsItemStack.invokeMethod("setTag", itemStack, tag);
+			return (ItemStack)craftItemStack.invokeMethod("asCraftMirror", null, itemStack);
+		} catch (Exception ex) {
+			return item;
+		}
 	}
 
 	@Override
-	public boolean equals(Object o) {
-		if (o == null) return false;
-		if (!(o instanceof ItemData)) return false;
-		ItemData pairo = (ItemData)o;
-		return this.itemId == pairo.getId() && this.itemData == pairo.getData();
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (!(obj instanceof ItemData)) return false;
+		if (this == obj) return true;
+		ItemData item = (ItemData)obj;
+		return this.getId() == item.getId() && this.getData() == item.getData();
+	}
+
+	public ItemStack getItem() {
+		ItemStack item = new ItemStack(id, data);
+		if (this.hasGlow()) item = addGlow(item);
+		return item;
 	}
 
 	public int getId() {
-		return itemId;
+		return this.getItem().getTypeId();
 	}
 
 	public short getData() {
-		return itemData;
+		return this.getItem().getDurability();
+	}
+
+	public boolean hasGlow() {
+		return this.glow;
 	}
 
 	@Override
 	public int hashCode() {
-		return (31 * itemId) ^ itemData;
+		return (31 * this.getId()) ^ this.getData();
+	}
+
+	public void removeGlow() {
+		this.glow = false;
 	}
 
 }
