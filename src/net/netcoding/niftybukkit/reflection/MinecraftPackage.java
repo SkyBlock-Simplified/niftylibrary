7a16fee7faaa0e72b4c8e43a0a87e127cbc7d040
Brian Graham <support@netcoding.net>
2015-03-10 23:26:00 -0400

    Added a MINECRAFT_VERSION to be used in 1.7 and 1.8 cross-compatibility checks

--- src/net/netcoding/niftybukkit/reflection/MinecraftPackage.java
+++ src/net/netcoding/niftybukkit/reflection/MinecraftPackage.java
@@ -6,18 +6,31 @@ import org.bukkit.Bukkit;
 
 public enum MinecraftPackage {
 
-	MINECRAFT_SERVER {
+	MINECRAFT_VERSION {
+
 		@Override
 		public String toString() {
-			return StringUtil.format("net.minecraft.server.{0}", Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3]);
+			return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
 		}
+
+	},
+
+	MINECRAFT_SERVER {
+
+		@Override
+		public String toString() {
+			return StringUtil.format("net.minecraft.server.{0}", MINECRAFT_VERSION);
+		}
+
 	},
 
 	CRAFTBUKKIT {
+
 		@Override
 		public String toString() {
 			return Bukkit.getServer().getClass().getPackage().getName();
 		}
+
 	};
 
 }
