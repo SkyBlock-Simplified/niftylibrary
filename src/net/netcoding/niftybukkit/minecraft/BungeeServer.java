8e0a3c766c4f06683cd463bde355fa21d46e4e03
Brian Graham <support@netcoding.net>
2015-03-16 02:02:43 -0400

    Override getPlayerList and add the temporary profiles if it's the current server

--- src/net/netcoding/niftybukkit/minecraft/BungeeServer.java
+++ src/net/netcoding/niftybukkit/minecraft/BungeeServer.java
@@ -1,13 +1,27 @@
 package net.netcoding.niftybukkit.minecraft;
 
 import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.Set;
+
+import net.netcoding.niftybukkit.mojang.MojangProfile;
+import net.netcoding.niftybukkit.util.concurrent.ConcurrentSet;
 
 public class BungeeServer extends MinecraftServer {
 
 	boolean loadedOnce = false;
+	final ConcurrentSet<MojangProfile> playersLeft = new ConcurrentSet<>();
 
 	BungeeServer(String serverName) {
 		this.serverName = serverName;
+	}
+
+	@Override
+	public Set<MojangProfile> getPlayerList() {
+		ConcurrentSet<MojangProfile> totalList = new ConcurrentSet<>();
+		totalList.addAll(this.playerList);
+		if (this.isCurrentServer()) totalList.addAll(this.playersLeft);
+		return Collections.unmodifiableSet(totalList);
 	}
 
 	@Override
