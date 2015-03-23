bc4ee5cd64d9e8d6e9118931f8d3401027d86092
Brian Graham <support@netcoding.net>
2015-03-23 02:50:02 -0400

    When reloading the sql info, on first load check the driver type and modify the driver and port

--- src/net/netcoding/niftybukkit/yaml/SQLConfig.java
+++ src/net/netcoding/niftybukkit/yaml/SQLConfig.java
@@ -33,13 +33,17 @@ public class SQLConfig<T extends SQLFactory> extends Config {
 
 	@Comment("Database Port")
 	@Path("sql.port")
-	protected int port = 3306;
+	protected int port = 0;
 
 	@Comment("Database Name")
 	@Path("sql.schema")
 	protected String schema = "";
 
-	public SQLConfig(JavaPlugin plugin, String fileName) throws SQLException {
+	public SQLConfig(JavaPlugin plugin, String fileName, String... header) throws SQLException {
+		this(plugin, fileName, false, header);
+	}
+
+	public SQLConfig(JavaPlugin plugin, String fileName, boolean skipFailedConversion, String... header) throws SQLException {
 		super(plugin, fileName);
 		this.reloadSQL();
 	}
@@ -70,12 +74,28 @@ public class SQLConfig<T extends SQLFactory> extends Config {
 
 	@SuppressWarnings("unchecked")
 	public final void reloadSQL() throws SQLException {
-		if (this.driver.equalsIgnoreCase("PostgreSQL"))
-			this.sql = (T)new PostgreSQL(this.getHost(), this.getPort(), this.getUser(), this.getPass(), this.getSchema());
-		else if (this.driver.equalsIgnoreCase("MSSQL"))
-			this.sql = (T)new SQLServer(this.getHost(), this.getPort(), this.getUser(), this.getPass(), this.getSchema());
-		else
-			this.sql = (T)new MySQL(this.getHost(), this.getPort(), this.getUser(), this.getPass(), this.getSchema());
+		if (this.driver.equalsIgnoreCase("sql")) {
+			if (this.sql instanceof PostgreSQL) {
+				this.driver = "postgresql";
+				this.port = PostgreSQL.DEFAULT_PORT;
+			} else if (this.sql instanceof SQLServer) {
+				this.driver = "mssql";
+				this.port = SQLServer.DEFAULT_PORT;
+			} else if (this.sql instanceof MySQL) {
+				this.driver = "mysql";
+				this.port = MySQL.DEFAULT_PORT;
+			}
+
+			if (!this.driver.equalsIgnoreCase("sql"))
+				this.save();
+		} else {
+			if (this.driver.equalsIgnoreCase("PostgreSQL"))
+				this.sql = (T)new PostgreSQL(this.getHost(), this.getPort(), this.getUser(), this.getPass(), this.getSchema());
+			else if (this.driver.equalsIgnoreCase("MSSQL"))
+				this.sql = (T)new SQLServer(this.getHost(), this.getPort(), this.getUser(), this.getPass(), this.getSchema());
+			else
+				this.sql = (T)new MySQL(this.getHost(), this.getPort(), this.getUser(), this.getPass(), this.getSchema());
+		}
 	}
 
 }
