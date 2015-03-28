829a18d52e1e9aa0bee59828bd7ca268368c90d8
Brian Graham <support@netcoding.net>
2015-03-27 21:07:41 -0400

    Now properly handles extending configurations that pick an extended wrapper, and not the default one

--- src/net/netcoding/niftybukkit/yaml/SQLConfig.java
+++ src/net/netcoding/niftybukkit/yaml/SQLConfig.java
@@ -81,35 +81,60 @@ public class SQLConfig<T extends SQLWrapper> extends Config {
 	@SuppressWarnings("unchecked")
 	private Class<T> getSuperClass() {
 		ParameterizedType superClass = (ParameterizedType)this.getClass().getGenericSuperclass();
-		return (Class<T>)superClass.getActualTypeArguments()[0];
+		return (Class<T>)(superClass.getActualTypeArguments().length == 0 ? SQLWrapper.class : superClass.getActualTypeArguments()[0]);
+	}
+
+	private void handleForcedWrapper(boolean loading) {
+		if (!SQLWrapper.class.equals(this.getSuperClass())) {
+			if (this.root.has("sql")) {
+				ConfigSection sql = (ConfigSection)this.root.get("sql");
+				sql.remove("driver");
+			}
+
+			if (loading) {
+				if (this.driver.equalsIgnoreCase("sql")) {
+					Class<T> clazz = this.getSuperClass();
+					this.driver = clazz.getSimpleName().toLowerCase();
+
+					if (this.port == 0) {
+						try {
+							Field portField = clazz.getField("DEFAULT_PORT");
+							this.port = portField.getInt(null);
+						} catch (Exception ex) { }
+					}
+				}
+			}
+		}
 	}
 
 	@Override
 	public void init() throws InvalidConfigurationException {
 		super.init();
-		this.initSQL();
+		//this.initSQL();
 	}
 
-	private void initSQL() throws InvalidConfigurationException {
+	/*private void initSQL() throws InvalidConfigurationException {
 		Class<?> clazz = this.getSuperClass();
 
-		if (this.driver.equalsIgnoreCase("sql")) {
-			try {
-				Field field = clazz.getField("DEFAULT_PORT");
-				this.port = field.getInt(null);
+		if (!SQLWrapper.class.equals(this.getSuperClass())) {
+			if (this.driver.equalsIgnoreCase("sql")) {
+				try {
+					Field portField = clazz.getField("DEFAULT_PORT");
+					this.port = portField.getInt(null);
 
-				if (PostgreSQL.class.isAssignableFrom(clazz))
-					this.driver = "postgresql";
-				else if (SQLServer.class.isAssignableFrom(clazz))
-					this.driver = "sqlserver";
-				else if (MySQL.class.isAssignableFrom(clazz))
-					this.driver = "mysql";
-			} catch (Exception ex) { }
+					if (PostgreSQL.class.isAssignableFrom(clazz))
+						this.driver = "postgresql";
+					else if (SQLServer.class.isAssignableFrom(clazz))
+						this.driver = "sqlserver";
+					else if (MySQL.class.isAssignableFrom(clazz))
+						this.driver = "mysql";
+				} catch (Exception ex) { }
 
-			if (!this.driver.equalsIgnoreCase("sql"))
-				this.save();
+				if (!this.driver.equalsIgnoreCase("sql"))
+					this.save();
+			}
 		}
-	}
+	}*/
 
 	protected void initFactory() throws SQLException {
 		if (this.driver.equalsIgnoreCase("PostgreSQL"))
@@ -129,6 +154,18 @@ public class SQLConfig<T extends SQLWrapper> extends Config {
 		} catch (SQLException sqlex) {
 			throw new InvalidConfigurationException("Invalid SQL Configuration!", sqlex);
 		}
+	}
+
+	@Override
+	protected void loadFromYaml() {
+		super.loadFromYaml();
+		this.handleForcedWrapper(true);
+	}
+
+	@Override
+	protected void saveToYaml() throws InvalidConfigurationException {
+		this.handleForcedWrapper(false);
+		super.saveToYaml();
 	}
 
 }
