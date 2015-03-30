8d7c46e10b73192141170e9a5e3601bb9f9f3c0e
Brian Graham <support@netcoding.net>
2015-03-30 02:10:06 -0400

    Added an enchantment database

---
+++ src/net/netcoding/niftybukkit/inventory/enchantments/EnchantmentData.java
@@ -0,0 +1,90 @@
+package net.netcoding.niftybukkit.inventory.enchantments;
+
+import java.util.List;
+
+import net.netcoding.niftybukkit.NiftyBukkit;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+
+public class EnchantmentData {
+
+	private final Enchantment enchantment;
+	private int userLevel = 1;
+
+	public EnchantmentData(Enchantment enchantment) {
+		this.enchantment = enchantment;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (obj == this) return true;
+		if (!(obj instanceof EnchantmentData)) return false;
+		EnchantmentData detail = (EnchantmentData)obj;
+		return detail.getEnchantment().equals(this.getEnchantment());
+	}
+
+	public void apply(ItemStack stack) {
+		this.apply(stack, false);
+	}
+
+	public void apply(ItemStack stack, boolean unsafe) {
+		int level = this.getUserLevel();
+
+		if (unsafe)
+			stack.addUnsafeEnchantment(this.getEnchantment(), level);
+		else {
+			level = (level > this.getMaxLevel() ? this.getMaxLevel() : level);
+
+			if (this.getEnchantment().canEnchantItem(stack)) {
+				for (Enchantment ench : stack.getEnchantments().keySet()) {
+					if (this.getEnchantment().conflictsWith(ench))
+						return;
+				}
+
+				stack.addEnchantment(this.getEnchantment(), level);
+			}
+		}
+	}
+
+	public Enchantment getEnchantment() {
+		return this.enchantment;
+	}
+
+	public int getId() {
+		return this.getEnchantment().getId();
+	}
+
+	public int getMaxLevel() {
+		return this.getEnchantment().getMaxLevel();
+	}
+
+	public String getName() {
+		return this.getEnchantment().getName();
+	}
+
+	public List<String> getNames() {
+		return NiftyBukkit.getEnchantmentDatabase().names(this.getEnchantment());
+	}
+
+	public int getUserLevel() {
+		return this.userLevel;
+	}
+
+	public int getStartLevel() {
+		return this.getEnchantment().getStartLevel();
+	}
+
+	@Override
+	public int hashCode() {
+		return this.getEnchantment().hashCode();
+	}
+
+	public void setUserLevel(int value) {
+		value = (value < this.getStartLevel() ? this.getStartLevel() : value);
+		value = (value > Short.MAX_VALUE ? Short.MAX_VALUE : value);
+		this.userLevel = value;
+	}
+
+}
---
+++ src/net/netcoding/niftybukkit/inventory/enchantments/EnchantmentDatabase.java
@@ -0,0 +1,209 @@
+package net.netcoding.niftybukkit.inventory.enchantments;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import net.netcoding.niftybukkit.minecraft.BukkitHelper;
+import net.netcoding.niftybukkit.util.ListUtil;
+import net.netcoding.niftybukkit.util.NumberUtil;
+import net.netcoding.niftybukkit.util.StringUtil;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EnchantmentDatabase extends BukkitHelper {
+
+	private final transient File enchantmentsFile;
+	private final transient Map<String, Integer> enchantments = new HashMap<>();
+	final transient Map<EnchantmentData, List<String>> names = new HashMap<>();
+	final transient Map<EnchantmentData, String> primaryName = new HashMap<>();
+
+	public EnchantmentDatabase(JavaPlugin plugin) {
+		this(plugin, "enchantments.csv");
+	}
+
+	public EnchantmentDatabase(JavaPlugin plugin, String fileName) {
+		super(plugin);
+		this.enchantmentsFile = new File(this.getPlugin().getDataFolder(), fileName);
+	}
+
+	public EnchantmentData get(final Enchantment enchantment) throws RuntimeException {
+		if (enchantment == null) throw new NullPointerException("The value for enchantment cannot be null!");
+		return this.get(enchantment.getName());
+	}
+
+	public EnchantmentData get(final String name) throws RuntimeException {
+		if (StringUtil.isEmpty(name)) throw new NullPointerException("The value for name cannot be null!");
+
+		for (EnchantmentData enchData : names.keySet()) {
+			for (String compare : enchData.getNames()) {
+				if (compare.equalsIgnoreCase(name))
+					return new EnchantmentData(enchData.getEnchantment());
+			}
+		}
+
+		for (EnchantmentData enchData : names.keySet()) {
+			for (String compare : enchData.getNames()) {
+				if (compare.startsWith(name.toLowerCase()))
+					return new EnchantmentData(enchData.getEnchantment());
+			}
+		}
+
+		return null;
+	}
+
+	public List<String> names(Enchantment enchantment) {
+		List<String> nameList = this.names.get(enchantment);
+		if (nameList.size() > 15) nameList = nameList.subList(0, 14);
+		return nameList;
+	}
+
+	public String name(Enchantment enchantment) {
+		return this.primaryName.get(enchantment);
+	}
+
+	public List<EnchantmentData> parse(String[] enchList) throws NumberFormatException {
+		if (ListUtil.isEmpty(enchList)) return Collections.emptyList();
+		List<EnchantmentData> enchDataList = new ArrayList<>();
+
+		for (int i = 0; i < enchList.length; i++) {
+			String ench = enchList[i];
+
+			if (ench.contains(":")) {
+				String[] split = ench.split(":");
+				EnchantmentData enchData = this.get(split[0]);
+				String enchLevel = split[1];
+
+				if (NumberUtil.isInt(enchLevel)) {
+					enchData.setUserLevel(Short.parseShort(enchLevel));
+					try { enchDataList.add(enchData); } catch (Exception ex) { }
+				} else
+					throw new NumberFormatException("Data value must be an integer, list or range!");
+			} else {
+				EnchantmentData enchData = this.get(ench);
+
+				if (NumberUtil.isInt(enchList[i + 1]))
+					enchData.setUserLevel(Short.parseShort(enchList[++i]));
+
+				try { enchDataList.add(enchData); } catch (Exception ex) { }
+			}
+		}
+
+		return Collections.unmodifiableList(enchDataList);
+	}
+
+	public List<EnchantmentData> parse(String enchStringList) throws NumberFormatException {
+		if (StringUtil.isEmpty(enchStringList)) return Collections.emptyList();
+		return this.parse(enchStringList.split("[,\\s]+"));
+	}
+
+	/**
+30.03 01:04:39 [Server] INFO LUCK : 61 : 3
+30.03 01:04:39 [Server] INFO LURE : 62 : 3
+30.03 01:04:39 [Server] INFO FIRE_ASPECT : 20 : 2
+30.03 01:04:39 [Server] INFO LOOT_BONUS_MOBS : 21 : 3
+30.03 01:04:39 [Server] INFO DAMAGE_ARTHROPODS : 18 : 5
+30.03 01:04:39 [Server] INFO ARROW_DAMAGE : 48 : 5
+30.03 01:04:39 [Server] INFO KNOCKBACK : 19 : 2
+30.03 01:04:39 [Server] INFO ARROW_KNOCKBACK : 49 : 2
+30.03 01:04:39 [Server] INFO DAMAGE_ALL : 16 : 5
+30.03 01:04:39 [Server] INFO ARROW_FIRE : 50 : 1
+30.03 01:04:39 [Server] INFO DAMAGE_UNDEAD : 17 : 5
+30.03 01:04:39 [Server] INFO ARROW_INFINITE : 51 : 1
+30.03 01:04:39 [Server] INFO DEPTH_STRIDER : 8 : 3
+30.03 01:04:39 [Server] INFO THORNS : 7 : 3
+30.03 01:04:39 [Server] INFO WATER_WORKER : 6 : 1
+30.03 01:04:39 [Server] INFO OXYGEN : 5 : 3
+30.03 01:04:39 [Server] INFO PROTECTION_PROJECTILE : 4 : 4
+30.03 01:04:39 [Server] INFO PROTECTION_EXPLOSIONS : 3 : 4
+30.03 01:04:39 [Server] INFO SILK_TOUCH : 33 : 1
+30.03 01:04:39 [Server] INFO PROTECTION_FALL : 2 : 4
+30.03 01:04:39 [Server] INFO DIG_SPEED : 32 : 5
+30.03 01:04:39 [Server] INFO PROTECTION_FIRE : 1 : 4
+30.03 01:04:39 [Server] INFO LOOT_BONUS_BLOCKS : 35 : 3
+30.03 01:04:39 [Server] INFO PROTECTION_ENVIRONMENTAL : 0 : 4
+30.03 01:04:39 [Server] INFO DURABILITY : 34 : 3
+	 */
+
+	public List<String> getLines() {
+		try {
+			try (InputStreamReader inputStream = (this.enchantmentsFile.exists() ? new FileReader(this.enchantmentsFile) : new InputStreamReader(this.getPlugin().getResource(this.enchantmentsFile.getName())))) {
+				try (BufferedReader reader = new BufferedReader(inputStream)) {
+					List<String> lines = new ArrayList<>();
+
+					do {
+						String line = reader.readLine();
+						if (StringUtil.isEmpty(line)) break;
+						lines.add(line);
+					} while (true);
+
+					return Collections.unmodifiableList(lines);
+				}
+			}
+		} catch (IOException ex) {
+			this.getLog().console(ex);
+			return Collections.emptyList();
+		}
+	}
+
+	public void save() {
+		this.save(false);
+	}
+
+	public void save(boolean replace) {
+		this.getPlugin().saveResource(this.enchantmentsFile.getName(), replace);
+	}
+
+	public void reload() {
+		final List<String> lines = this.getLines();
+		if (lines.isEmpty()) return;
+
+		this.names.clear();
+		this.primaryName.clear();
+		LengthCompare compare = new LengthCompare();
+
+		for (String line : lines) {
+			line = line.trim().toLowerCase(Locale.ENGLISH);
+			if (line.length() > 0 && line.charAt(0) == '#') continue;
+			final String[] parts = line.split(",");
+			if (parts.length < 3) continue;
+			String itemName = parts[0].toLowerCase(Locale.ENGLISH);
+			final int numeric = Integer.parseInt(parts[1]);
+			this.enchantments.put(itemName, numeric);
+			EnchantmentData enchData = new EnchantmentData(Enchantment.getById(numeric));
+
+			if (!this.names.containsKey(enchData)) {
+				this.names.put(enchData, new ArrayList<>(Arrays.asList(itemName)));
+				this.primaryName.put(enchData, itemName);
+			} else {
+				this.names.get(enchData).add(itemName);
+				Collections.sort(this.names.get(enchData), compare);
+			}
+		}
+	}
+
+	private class LengthCompare implements Comparator<String> {
+
+		public LengthCompare() {
+			super();
+		}
+
+		@Override
+		public int compare(String s1, String s2) {
+			return s1.length() - s2.length();
+		}
+
+	}
+
+}
