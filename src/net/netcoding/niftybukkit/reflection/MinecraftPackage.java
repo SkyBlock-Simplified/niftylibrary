fbc36c763110728013df6bee4b4bdaf98e2eabc4
Brian Graham <support@netcoding.net>
2015-04-21 03:07:10 -0400

    Added IS_SPIGOT boolean check

--- src/net/netcoding/niftybukkit/reflection/MinecraftPackage.java
+++ src/net/netcoding/niftybukkit/reflection/MinecraftPackage.java
@@ -13,5 +13,18 @@ public class MinecraftPackage {
 	private static final String MINECRAFT_VERSION_NUMBER = MINECRAFT_VERSION.replaceAll("^v", "").replaceAll("R", "").replace("_", ".");
 	public static final boolean IS_PRE_1_8 = new VersionUtil(MINECRAFT_VERSION_NUMBER).compareTo(new VersionUtil("1.8.0")) < 0;
 	public static final boolean IS_PRE_1_8_3 = new VersionUtil(MINECRAFT_VERSION_NUMBER).compareTo(new VersionUtil("1.8.2")) < 0;
+	public static final boolean IS_SPIGOT;
+
+	static {
+		boolean spigot = false;
+
+		try {
+			Reflection spigotPlayerRef = new Reflection("Spigot", "Player", "org.bukkit.entity");
+			spigotPlayerRef.getClazz();
+			spigot = true;
+		} catch (Exception ex) { }
+
+		IS_SPIGOT = spigot;
+	}
 
 }
