a8c99b1ba2f5b7e25b6b2e174aa677cd5b4df9f8
Brian Graham <support@netcoding.net>
2016-01-16 17:31:23 -0500

    Fixed map import

--- src/main/java/net/netcoding/niftybukkit/yaml/converters/Location.java
+++ src/main/java/net/netcoding/niftybukkit/yaml/converters/Location.java
@@ -1,14 +1,13 @@
 package net.netcoding.niftybukkit.yaml.converters;
 
-import java.lang.reflect.ParameterizedType;
-import java.util.HashMap;
-
 import net.netcoding.niftycore.yaml.ConfigSection;
 import net.netcoding.niftycore.yaml.InternalConverter;
 import net.netcoding.niftycore.yaml.converters.Converter;
-import net.netcoding.niftycore.yaml.converters.Map;
-
 import org.bukkit.Bukkit;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("unchecked")
 public class Location extends Converter {
@@ -19,7 +18,7 @@ public class Location extends Converter {
 
 	@Override
 	public Object fromConfig(Class<?> type, Object section, ParameterizedType genericType) throws Exception {
-		java.util.Map<String, Object> map = (section instanceof Map) ? (java.util.Map<String, Object>)section : (java.util.Map<String, Object>)((ConfigSection)section).getRawMap();
+		Map<String, Object> map = (section instanceof Map) ? (Map<String, Object>)section : (Map<String, Object>)((ConfigSection)section).getRawMap();
 		float yaw = (map.get("yaw") instanceof Double) ? ((Double)map.get("yaw")).floatValue() : (Float)map.get("yaw");
 		float pitch = (map.get("pitch") instanceof Double) ? ((Double)map.get("pitch")).floatValue() : (Float)map.get("pitch");
 		return new org.bukkit.Location(Bukkit.getWorld((String)map.get("world")), (Double)map.get("x"), (Double)map.get("y"), (Double)map.get("z"), yaw, pitch);
@@ -28,7 +27,7 @@ public class Location extends Converter {
 	@Override
 	public Object toConfig(Class<?> type, Object obj, ParameterizedType genericType) throws Exception {
 		org.bukkit.Location location = (org.bukkit.Location)obj;
-		java.util.Map<String, Object> saveMap = new HashMap<>();
+		Map<String, Object> saveMap = new HashMap<>();
 		saveMap.put("world", location.getWorld().getName());
 		saveMap.put("x", location.getX());
 		saveMap.put("y", location.getY());
