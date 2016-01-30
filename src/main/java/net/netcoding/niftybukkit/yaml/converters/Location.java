3ab2a14b6e7135298b3aaf0f5b5344c365dd64b5
Brian Graham <support@netcoding.net>
2016-01-30 00:01:42 -0500

    Use %world% to specify default world (multiple servers running in same directory)

--- src/main/java/net/netcoding/niftybukkit/yaml/converters/Location.java
+++ src/main/java/net/netcoding/niftybukkit/yaml/converters/Location.java
@@ -1,9 +1,11 @@
 package net.netcoding.niftybukkit.yaml.converters;
 
+import net.netcoding.niftycore.util.concurrent.ConcurrentSet;
 import net.netcoding.niftycore.yaml.ConfigSection;
 import net.netcoding.niftycore.yaml.InternalConverter;
 import net.netcoding.niftycore.yaml.converters.Converter;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 
 import java.lang.reflect.ParameterizedType;
 import java.util.HashMap;
@@ -11,6 +13,8 @@ import java.util.Map;
 
 @SuppressWarnings("unchecked")
 public class Location extends Converter {
+
+	private final static ConcurrentSet<org.bukkit.Location> WILDCARDS = new ConcurrentSet<>();
 
 	public Location(InternalConverter converter) {
 		super(converter);
@@ -21,14 +25,22 @@ public class Location extends Converter {
 		Map<String, Object> map = (Map<String, Object>)((section instanceof Map) ? section : ((ConfigSection)section).getRawMap());
 		float yaw = (map.get("yaw") instanceof Double) ? ((Double)map.get("yaw")).floatValue() : (Float)map.get("yaw");
 		float pitch = (map.get("pitch") instanceof Double) ? ((Double)map.get("pitch")).floatValue() : (Float)map.get("pitch");
-		return new org.bukkit.Location(Bukkit.getWorld((String)map.get("world")), (Double)map.get("x"), (Double)map.get("y"), (Double)map.get("z"), yaw, pitch);
+		String mapName = (String)map.get("world");
+		boolean wildcard = "%world%".equalsIgnoreCase(mapName);
+		World world = (wildcard ? Bukkit.getWorlds().get(0) : Bukkit.getWorld(mapName));
+		org.bukkit.Location location = new org.bukkit.Location(world, (Double)map.get("x"), (Double)map.get("y"), (Double)map.get("z"), yaw, pitch);
+
+		if (wildcard)
+			WILDCARDS.add(location);
+
+		return location;
 	}
 
 	@Override
 	public Object toConfig(Class<?> type, Object obj, ParameterizedType genericType) throws Exception {
 		org.bukkit.Location location = (org.bukkit.Location)obj;
 		Map<String, Object> saveMap = new HashMap<>();
-		saveMap.put("world", location.getWorld().getName());
+		saveMap.put("world", (WILDCARDS.contains(location) ? "%world%" : location.getWorld().getName()));
 		saveMap.put("x", location.getX());
 		saveMap.put("y", location.getY());
 		saveMap.put("z", location.getZ());
