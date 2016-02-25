14aeb816fae33740362134c90574b05cc9cc5c26
Brian Graham <support@netcoding.net>
2016-02-25 17:40:13 -0500

    Now supports checking equals with BukkitMojangProfile

--- src/main/java/net/netcoding/niftybukkit/mojang/BukkitMojangCache.java
+++ src/main/java/net/netcoding/niftybukkit/mojang/BukkitMojangCache.java
@@ -1,7 +1,6 @@
 package net.netcoding.niftybukkit.mojang;
 
 import net.netcoding.niftybukkit.minecraft.BukkitHelper;
-
 import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,9 +17,14 @@ public class BukkitMojangCache<T extends BukkitMojangProfile> extends BukkitHelp
 	public boolean equals(Object obj) {
 		if (obj == null) return false;
 		if (this == obj) return true;
-		if (!(obj instanceof BukkitMojangCache)) return false;
-		BukkitMojangCache cache = (BukkitMojangCache)obj;
-		return this.getProfile().equals(cache.getProfile());
+		BukkitMojangProfile profile = null;
+
+		if (obj instanceof BukkitMojangCache)
+			profile = ((BukkitMojangCache)obj).getProfile();
+		else if (obj instanceof BukkitMojangProfile)
+			profile = (BukkitMojangProfile)obj;
+
+		return this.getProfile().equals(profile);
 	}
 
 	public T getProfile() {
