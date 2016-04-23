efe1eedaf7e946241457ff68fe29cdea6bfccaf4
Brian Graham <support@netcoding.net>
2016-04-23 18:46:52 -0400

    Hide the version string

--- src/main/java/net/netcoding/niftybukkit/reflection/MinecraftPackage.java
+++ src/main/java/net/netcoding/niftybukkit/reflection/MinecraftPackage.java
@@ -9,7 +9,7 @@ import org.bukkit.Bukkit;
 public class MinecraftPackage {
 
 	public static final String CRAFTBUKKIT = Bukkit.getServer().getClass().getPackage().getName();
-	public static final String MINECRAFT_VERSION = CRAFTBUKKIT.split("\\.")[3];
+	private static final String MINECRAFT_VERSION = CRAFTBUKKIT.split("\\.")[3];
 	public static final String MINECRAFT_SERVER = StringUtil.format("net.minecraft.server.{0}", MINECRAFT_VERSION);
 	private static final String MINECRAFT_VERSION_NUMBER = MINECRAFT_VERSION.replaceAll("^v", "").replaceAll("R", "").replace("_", ".");
 	public static final boolean IS_PRE_1_8 = new VersionUtil(MINECRAFT_VERSION_NUMBER).compareTo(new VersionUtil("1.8.0")) < 0;
