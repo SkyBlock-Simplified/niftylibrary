ec66678e351a929ad351d13a05cad7bd19062409
Brian Graham <support@netcoding.net>
2016-05-09 19:13:45 -0400

    Add ItemFlag converter if server is newer than 1.7.10

--- src/main/java/net/netcoding/niftybukkit/yaml/BukkitSQLConfig.java
+++ src/main/java/net/netcoding/niftybukkit/yaml/BukkitSQLConfig.java
@@ -1,6 +1,8 @@
 package net.netcoding.niftybukkit.yaml;
 
+import net.netcoding.niftybukkit.reflection.MinecraftProtocol;
 import net.netcoding.niftybukkit.yaml.converters.Block;
+import net.netcoding.niftybukkit.yaml.converters.ItemFlag;
 import net.netcoding.niftybukkit.yaml.converters.ItemStack;
 import net.netcoding.niftybukkit.yaml.converters.Location;
 import net.netcoding.niftycore.database.factory.SQLWrapper;
@@ -19,6 +21,9 @@ public abstract class BukkitSQLConfig<T extends SQLWrapper> extends SQLYamlConfi
 		this.addCustomConverter(Block.class);
 		this.addCustomConverter(ItemStack.class);
 		this.addCustomConverter(Location.class);
+
+		if (MinecraftProtocol.getCurrentProtocol() >= MinecraftProtocol.v1_8_pre1.getProtocol())
+			this.addCustomConverter(ItemFlag.class);
 	}
 
 }
