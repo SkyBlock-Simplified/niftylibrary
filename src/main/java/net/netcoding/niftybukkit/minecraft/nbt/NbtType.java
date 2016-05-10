6b30077ded5cce4729165b80dc2d5f41a32456fa
Brian Graham <support@netcoding.net>
2016-05-09 20:59:16 -0400

    Passing a collection, set or object array will now default to an NbtList

--- src/main/java/net/netcoding/niftybukkit/minecraft/nbt/NbtType.java
+++ src/main/java/net/netcoding/niftybukkit/minecraft/nbt/NbtType.java
@@ -1,7 +1,9 @@
 package net.netcoding.niftybukkit.minecraft.nbt;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 enum NbtType {
 
@@ -16,6 +18,9 @@ enum NbtType {
 	TAG_INT_ARRAY(11, int[].class),
 	TAG_STRING(8, String.class),
 	TAG_LIST(9, List.class),
+	TAG_SET(9, Set.class),
+	TAG_COLLECTION(9, Collection.class),
+	TAG_ARRAY(9, Object[].class),
 	TAG_COMPOUND(10, Map.class),
 	TAG_BOOLEAN(0xCAFE_BABE, boolean.class);
 
@@ -34,7 +39,7 @@ enum NbtType {
 	String getFieldName() {
 		if (this == TAG_COMPOUND)
 			return "map";
-		else if (this == TAG_LIST)
+		else if (this == TAG_LIST || this == TAG_SET || this == TAG_COLLECTION || this == TAG_ARRAY)
 			return "list";
 		else
 			return "data";
