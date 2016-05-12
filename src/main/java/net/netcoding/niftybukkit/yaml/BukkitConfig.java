7b8189aabe81a5dff51099cd8844cb557b603fa1
Brian Graham <support@netcoding.net>
2016-05-12 00:59:30 -0400

    Removed ItemFlag converter

--- src/main/java/net/netcoding/niftybukkit/yaml/BukkitConfig.java
+++ src/main/java/net/netcoding/niftybukkit/yaml/BukkitConfig.java
@@ -1,8 +1,6 @@
 package net.netcoding.niftybukkit.yaml;
 
-import net.netcoding.niftybukkit.reflection.MinecraftProtocol;
 import net.netcoding.niftybukkit.yaml.converters.Block;
-import net.netcoding.niftybukkit.yaml.converters.ItemFlag;
 import net.netcoding.niftybukkit.yaml.converters.ItemStack;
 import net.netcoding.niftybukkit.yaml.converters.Location;
 import net.netcoding.niftycore.yaml.YamlConfig;
@@ -20,9 +18,6 @@ public class BukkitConfig extends YamlConfig {
 		this.addCustomConverter(Block.class);
 		this.addCustomConverter(ItemStack.class);
 		this.addCustomConverter(Location.class);
-
-		if (MinecraftProtocol.getCurrentProtocol() >= MinecraftProtocol.v1_8_pre1.getProtocol())
-			this.addCustomConverter(ItemFlag.class);
 	}
 
 }
