1f48796f2113730c5749b09aa030c8d8e1dd9fde
Brian Graham <support@netcoding.net>
2016-05-14 15:42:46 -0400

    Should always be cancellable

--- src/main/java/net/netcoding/niftybukkit/minecraft/inventory/events/InventoryItemInteractEvent.java
+++ src/main/java/net/netcoding/niftybukkit/minecraft/inventory/events/InventoryItemInteractEvent.java
@@ -4,12 +4,12 @@ import net.netcoding.niftybukkit.minecraft.items.ItemData;
 import net.netcoding.niftybukkit.mojang.BukkitMojangProfile;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-public class InventoryItemInteractEvent extends InventoryCancellableEvent {
+public class InventoryItemInteractEvent extends InventoryEvent {
 
 	private final transient PlayerInteractEvent event;
 
 	public InventoryItemInteractEvent(BukkitMojangProfile profile, PlayerInteractEvent event) {
-		super(profile);
+		super(profile, profile);
 		this.event = event;
 	}
 
