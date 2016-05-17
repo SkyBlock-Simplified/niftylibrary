82e079050f88ab128173a35fee4bc59212bdffe9
Brian Graham <support@netcoding.net>
2016-05-17 16:11:38 -0400

    Loading map now works

--- src/main/java/net/netcoding/niftybukkit/yaml/converters/ItemStack.java
+++ src/main/java/net/netcoding/niftybukkit/yaml/converters/ItemStack.java
@@ -3,11 +3,11 @@ package net.netcoding.niftybukkit.yaml.converters;
 import net.netcoding.niftybukkit.NiftyBukkit;
 import net.netcoding.niftybukkit.minecraft.items.ItemData;
 import net.netcoding.niftybukkit.minecraft.items.enchantments.EnchantmentData;
+import net.netcoding.niftybukkit.minecraft.nbt.NbtCompound;
 import net.netcoding.niftybukkit.reflection.MinecraftProtocol;
 import net.netcoding.niftycore.util.ListUtil;
 import net.netcoding.niftycore.util.RegexUtil;
 import net.netcoding.niftycore.util.concurrent.linked.ConcurrentLinkedMap;
-import net.netcoding.niftycore.yaml.ConfigSection;
 import net.netcoding.niftycore.yaml.InternalConverter;
 import net.netcoding.niftycore.yaml.converters.Converter;
 import org.bukkit.enchantments.Enchantment;
@@ -28,9 +28,10 @@ public class ItemStack extends Converter {
 
 	@Override
 	public Object fromConfig(Class<?> type, Object section, ParameterizedType genericType) throws Exception {
-		Map<String, Object> itemMap = (section instanceof Map ? (Map<String, Object>)section : (Map<String, Object>)((ConfigSection)section).getRawMap());
-		Map<String, Object> metaMap = (itemMap.get("meta") instanceof Map ? (Map<String, Object>)itemMap.get("meta") : (Map<String, Object>)((ConfigSection)itemMap.get("meta")).getRawMap());
-		ItemData itemData = new ItemData(NiftyBukkit.getItemDatabase().get((String)itemMap.get("id")));
+		Converter mapConverter = this.getConverter(Map.class);
+		Map<String, Object> itemMap = (Map<String, Object>)mapConverter.fromConfig(Map.class, section, null);
+		Map<String, Object> metaMap = (Map<String, Object>)mapConverter.fromConfig(Map.class, itemMap.get("meta"), null);
+		ItemData itemData = NiftyBukkit.getItemDatabase().get((String)itemMap.get("id"));
 		itemData.setAmount((int)itemMap.get("amount"));
 		ItemMeta itemMeta = itemData.getItemMeta();
 
@@ -43,15 +44,14 @@ public class ItemStack extends Converter {
 
 			if (MinecraftProtocol.isPost1_8()) {
 				if (metaMap.containsKey("flags")) {
-					Converter setConverter = this.getConverter(Set.class);
 					ParameterizedType flagType = (ParameterizedType)org.bukkit.inventory.ItemFlag.class.getGenericSuperclass();
-					Set<org.bukkit.inventory.ItemFlag> itemFlags = (Set<org.bukkit.inventory.ItemFlag>)setConverter.fromConfig(Set.class, metaMap.get("flags"), flagType);
+					Set<org.bukkit.inventory.ItemFlag> itemFlags = (Set<org.bukkit.inventory.ItemFlag>)this.getConverter(Set.class).fromConfig(Set.class, metaMap.get("flags"), flagType);
 					itemMeta.addItemFlags(ListUtil.toArray(itemFlags, org.bukkit.inventory.ItemFlag.class));
 				}
 			}
 
 			if (metaMap.containsKey("enchantments")) {
-				Map<String, Integer> enchMap = (metaMap.get("enchantments") instanceof Map ? (Map<String, Integer>)metaMap.get("enchantments") : (Map<String, Integer>)((ConfigSection)metaMap.get("enchantments")).getRawMap());
+				Map<String, Integer> enchMap = (Map<String, Integer>)mapConverter.fromConfig(Map.class, metaMap.get("enchantments"), null);
 
 				for (Map.Entry<String, Integer> enchantment : enchMap.entrySet()) {
 					EnchantmentData enchantmentData = new EnchantmentData(Enchantment.getByName(enchantment.getKey()));
@@ -62,7 +62,7 @@ public class ItemStack extends Converter {
 		}
 
 		if (itemMap.containsKey("nbt")) {
-			Map<String, Object> nbtMap = (itemMap.get("nbt") instanceof Map ? (Map<String, Object>)itemMap.get("nbt") : (Map<String, Object>)((ConfigSection)itemMap.get("nbt")).getRawMap());
+			Map<String, Object> nbtMap = (Map<String, Object>)mapConverter.fromConfig(Map.class, itemMap.get("nbt"), null);
 			itemData.getNbt().putAll(nbtMap);
 		}
 
@@ -74,7 +74,6 @@ public class ItemStack extends Converter {
 	public Object toConfig(Class<?> type, Object obj, ParameterizedType genericType) throws Exception {
 		ItemData itemData = new ItemData((org.bukkit.inventory.ItemStack)obj);
 		ItemMeta itemMeta = itemData.getItemMeta();
-		Converter listConverter = this.getConverter(List.class);
 		ConcurrentLinkedMap<String, Object> saveMap = new ConcurrentLinkedMap<>();
 		ConcurrentLinkedMap<String, Object> meta = new ConcurrentLinkedMap<>();
 		ConcurrentLinkedMap<String, Integer> enchantments = new ConcurrentLinkedMap<>();
@@ -90,27 +89,26 @@ public class ItemStack extends Converter {
 				lore.set(i, RegexUtil.replace(lore.get(i), RegexUtil.VANILLA_PATTERN, "&$1"));
 		}
 
-		meta.put("lore", listConverter.toConfig(List.class, lore, null));
+		meta.put("lore", this.getConverter(List.class).toConfig(List.class, lore, null));
 
 		for (Map.Entry<Enchantment, Integer> enchantment : itemData.getEnchantments().entrySet())
 			enchantments.put(enchantment.getKey().getName(), enchantment.getValue());
 
 		if (MinecraftProtocol.isPost1_8()) {
-			Converter setConverter = this.getConverter(Set.class);
 			ParameterizedType flagType = (ParameterizedType)org.bukkit.inventory.ItemFlag.class.getGenericSuperclass();
-			Set<org.bukkit.inventory.ItemFlag> itemFlags = (Set<org.bukkit.inventory.ItemFlag>)setConverter.toConfig(Set.class, itemMeta.getItemFlags(), flagType);
 
-			if (!itemFlags.isEmpty())
-				meta.put("flags", itemFlags);
+			if (!itemMeta.getItemFlags().isEmpty())
+				meta.put("flags", this.getConverter(Set.class).toConfig(Set.class, itemMeta.getItemFlags(), flagType));
 		}
 
 		if (!enchantments.isEmpty())
-			meta.put("enchantments", enchantments);
+			meta.put("enchantments", this.getConverter(Map.class).toConfig(Map.class, enchantments, null));
 
 		saveMap.put("meta", meta);
+		NbtCompound clone = itemData.getNbt();
 
-		if (itemData.getNbt().notEmpty())
-			saveMap.put("nbt", itemData.getNbt());
+		if (clone.notEmpty())
+			saveMap.put("nbt", this.getConverter(Map.class).toConfig(Map.class, clone, null));
 
 		return saveMap;
 	}
