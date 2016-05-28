70e0eeb46651fe246eaf29ea36aa7776b5f43d75
Brian Graham <support@netcoding.net>
2016-05-28 17:22:16 -0400

    Moved resource files Added test classes

---
+++ CraftBukkit/src/main/java/net/netcoding/niftycraftbukkit/NiftyCraftBukkit.java
@@ -0,0 +1,13 @@
+package net.netcoding.niftycraftbukkit;
+
+import net.netcoding.niftybukkit.util.json.BukkitJsonMessage;
+import net.netcoding.niftycore.util.StringUtil;
+
+public class NiftyCraftBukkit {
+
+	public NiftyCraftBukkit() {
+		BukkitJsonMessage json = new BukkitJsonMessage();
+		StringUtil util = new StringUtil();
+	}
+
+}
