9952c28582c6b9bd71689f8504c59e6ea3682b91
Brian Graham <support@netcoding.net>
2016-07-22 15:14:22 -0400

    Removed

--- Sponge/src/main/java-templates/net/netcoding/nifty/sponge/NiftySponge.java
+++ Sponge/src/main/java-templates/net/netcoding/nifty/sponge/NiftySponge.java
@@ -1,10 +1,7 @@
 package net.netcoding.nifty.sponge;
 
 import net.netcoding.nifty.common.api.plugin.MinecraftPlugin;
-import net.netcoding.nifty.core.api.plugin.annotations.Dependency;
-import net.netcoding.nifty.core.api.plugin.annotations.Plugin;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
@@ -16,13 +13,12 @@ import org.spongepowered.api.text.Text;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
-@org.spongepowered.api.plugin.Plugin(id = "niftybukkit", name = "NiftyBukkit", version = "${project.version}", dependencies = {
-		@org.spongepowered.api.plugin.Dependency(id = "ProtocolLib", optional = true),
+import java.net.URL;
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
+
+@org.spongepowered.api.plugin.Plugin(id = "${id}", name = "${name}", version = "${version}", dependencies = {
 		@org.spongepowered.api.plugin.Dependency(id = "Vault", optional = true)
-})
-@Plugin(id = "nifty${project.artifactId}", name = "Nifty${project.name}", version = "${project.version}", dependencies = {
-		@Dependency(id = "ProtocolLib", optional = true),
-		@Dependency(id = "Vault", optional = true)
 })
 public final class NiftySponge extends MinecraftPlugin {
 
@@ -32,23 +28,73 @@ public final class NiftySponge extends MinecraftPlugin {
 	// Commands: https://docs.spongepowered.org/master/en/plugin/commands/childcommands.html
 	// Bukkit -> Sponge: https://docs.google.com/spreadsheets/d/1phlRmbPHCVhRIaZBi9xMjUuiDQkW-j8rSEV_tQnD47E/edit#gid=0
 	// Messages: https://forums.spongepowered.org/t/send-a-message-to-bungeecord/7119
-	// Dependency Sorting: http://yongouyang.blogspot.com/2013/03/sorting-objects-with-one-way-dependency.html
 	// NbtTranslator: https://github.com/SpongePowered/SpongeCommon/blob/master/src/main/java/org/spongepowered/common/data/persistence/NbtTranslator.java
 	// Nbt?: https://github.com/SpongePowered/SpongeCommon/blob/master/src/main/java/org/spongepowered/common/mixin/core/
 
+/*
+Processing Class: net.minecraft.item.ItemStack : zx.class
+Processing Field: field_111284_a: java.text.DecimalFormat
+Processing Field: field_77994_a: int
+Processing Field: field_77992_b: int
+Processing Field: field_151002_e: net.minecraft.item.Item
+Processing Field: field_77990_d: net.minecraft.nbt.NBTTagCompound : dn.class
+Processing Field: field_77991_e: int
+Processing Field: field_82843_f: net.minecraft.entity.item.EntityItemFrame
+Processing Field: field_179552_h: net.minecraft.block.Block
+Processing Field: field_179553_i: boolean
+Processing Field: field_179550_j: net.minecraft.block.Block
+Processing Field: field_179551_k: boolean
+Processing Field: delegate: net.minecraftforge.fml.common.registry.RegistryDelegate
+Processing Field: capabilities: net.minecraftforge.common.capabilities.CapabilityDispatcher
+Processing Field: capNBT: net.minecraft.nbt.NBTTagCompound : can ignore??
+Processing Field: manipulators: java.util.List
+Processing Field: observers: java.util.List
+*/
+
 	@Override
 	public void onEnable() {
-		ItemStack stack = Sponge.getRegistry().createBuilder(ItemStack.Builder.class).itemType(ItemTypes.BOOK).build();
-		ItemStack stack2 = ItemStack.of(ItemTypes.BOOK, 1);
-		stack.offer(Keys.DISPLAY_NAME, Text.of("Wtf"));
-		stack.setQuantity(5);
+		/*ItemStack book = Sponge.getRegistry().createBuilder(ItemStack.Builder.class).itemType(ItemTypes.BOOK).build();
+		book.offer(Keys.DISPLAY_NAME, Text.of("Wtf"));
+		book.setQuantity(5);*/
+
+		ItemStack skull = ItemStack.of(ItemTypes.SKULL, 1);
+		this.check(skull);
 	}
 
+	public void check(Object obj) {
+		this.recurse(obj.getClass());
+	}
+
+	public void recurse(Class<?> clazz) {
+		System.out.println("Processing Class: " + clazz.getName() + ":" + this.getClazzLocation(clazz).toString());
+
+		for (java.lang.reflect.Field field : clazz.getDeclaredFields()) {
+			System.out.println("Processing Field: " + field.getName() + ": " + field.getType().getName());
+		}
+
+		if (clazz.getSuperclass() != null)
+			this.recurse(clazz.getSuperclass());
+	}
+
+	public final URL getClazzLocation(Class<?> clazz) {
+		ProtectionDomain domain = clazz.getProtectionDomain();
+
+		if (domain != null) {
+			CodeSource source = domain.getCodeSource();
+
+			if (source != null)
+				return source.getLocation();
+		}
+
+		return null;
+	}
+
+	@SuppressWarnings("OptionalGetWithoutIsPresent")
 	public void WIP() {
 		// Sponge.getGame().getChannelRegistrar()
 		// org.spongepowered.api.event.server.ClientPingServerEvent
 		// net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent
-		Player p = null;
+		Player p = Sponge.getServer().getPlayer("CraftedFury").get();
 		Location<World> loc = p.getLocation();
 		p.getLocation().getBlockX();
 		World world = Sponge.getServer().getWorlds().iterator().next();
