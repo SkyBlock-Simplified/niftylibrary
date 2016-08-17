b1fc3ec9fcffbee637864d5c71a7654977b66a5b
Brian Graham <support@netcoding.net>
2016-08-17 17:05:18 -0400

    Can now change logging colors from plugin class

--- Common/src/main/java/net/netcoding/nifty/common/api/plugin/MinecraftPlugin.java
+++ Common/src/main/java/net/netcoding/nifty/common/api/plugin/MinecraftPlugin.java
@@ -2,6 +2,7 @@ package net.netcoding.nifty.common.api.plugin;
 
 import net.netcoding.nifty.common.Nifty;
 import net.netcoding.nifty.common.minecraft.Server;
+import net.netcoding.nifty.core.api.color.ChatColor;
 import net.netcoding.nifty.core.api.plugin.Plugin;
 import net.netcoding.nifty.core.api.plugin.PluginDescription;
 
@@ -14,7 +15,7 @@ public abstract class MinecraftPlugin extends MinecraftCore implements Plugin<Mi
 			return false;
 		else {
 			MinecraftPlugin other = (MinecraftPlugin)obj;
-			return other.getName().equalsIgnoreCase(this.getName());
+			return other.getClass().equals(this.getClass());
 		}
 	}
 
@@ -33,19 +34,29 @@ public abstract class MinecraftPlugin extends MinecraftCore implements Plugin<Mi
 	}
 
 	public final int hashCode() {
-		return 32 * this.getName().hashCode();
+		return 32 * this.getClass().hashCode();
 	}
 
 	public final boolean isEnabled() {
 		return Nifty.getPluginManager().isEnabled(this.getDesc().getName());
 	}
 
-	public void onEnable() { }
+	protected void onEnable() { }
 
-	public void onDisable() { }
+	protected void onDisable() { }
 
 	protected final void setEnabled(boolean value) {
 		Nifty.getPluginManager().setEnabled(this, value);
+	}
+
+	public final void setLogColors(ChatColor color, ChatColor important) {
+		this.setLogColors(color, important, this.getLog().getBracket());
+	}
+
+	public final void setLogColors(ChatColor color, ChatColor important, ChatColor bracket) {
+		this.getLog().setBracket(bracket);
+		this.getLog().setColor(color);
+		this.getLog().setImportant(important);
 	}
 
 }
